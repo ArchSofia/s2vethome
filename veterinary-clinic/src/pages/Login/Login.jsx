d70aef0fc06a775ef2bcbdc52041b6bbac4fd925
ArchSofia <sofiadiazvaldez@gmail.com>
2023-09-27 11:49:10 -0300

    Agrega footer a login

---
+++ veterinary-clinic/src/pages/Login/Login.jsx
@@ -0,0 +1,122 @@
+import React, { useContext, useState } from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import "./Login.css";
+import { IoMdArrowRoundBack } from "react-icons/io";
+import { MdOutlineError } from "react-icons/md";
+import { IoMdEye } from "react-icons/io";
+import { AiFillEyeInvisible } from "react-icons/ai";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { UserContext } from "../../context/UserContext";
+import Footer from "../../components/Footer/Footer";
+
+const Login = () => {
+	const navigate = useNavigate();
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [showPassword, setShowPassword] = useState(false);
+
+	const [error, setError] = useState(false);
+	const [errorMessage, setErrorMessage] = useState("");
+
+	const { setIsLogged, findUserByEmail } = useContext(UserContext);
+
+	const goBack = () => {
+		navigate(-1);
+	};
+
+	const handleIcon = () => {
+		setShowPassword(!showPassword);
+	};
+
+	/* validar que el usuario haya ingresado la info en los inputs de email y username. 
+    Si los datos son válidos, el servidor devuelve un token. Sino, muestra un msj de error */
+	const handleLoginUser = (e) => {
+		e.preventDefault();
+		if (email && password) {
+			axios
+				.post("http://localhost:8080/login", {
+					email,
+					password,
+				})
+				.then((response) => {
+					console.log("usuario logueado correctamente", response.data);
+					if (response.status === 200 || response.status === 201) {
+						sessionStorage.setItem("token", response.data.token);
+						sessionStorage.setItem("email", email);
+						setIsLogged(true);
+						//hacer el llamado de la funcion que busca al usuario por su email, trae toda su info y la guarda:
+						findUserByEmail(email);
+						navigate("/");
+					}
+				})
+				.catch((error) => console.log("Hubo un error", error));
+		} else {
+			console.log("datos incorrectos");
+			setError(true);
+			setErrorMessage(`Email y/o contraseña inválidos.\n¡Inténtalo de nuevo!`);
+		}
+	};
+
+	return (
+		<>
+			<Navbar />
+			<div className="container-login-form">
+				<div className="icon-direction">
+					<IoMdArrowRoundBack className="arrow" onClick={goBack} />
+				</div>
+				<div className="ctn-login">
+					<h1 className="login-title">Iniciá sesión</h1>
+					{error && (
+						<div className="error-container">
+							<MdOutlineError className="icon-error" />
+							<p>{errorMessage}</p>
+						</div>
+					)}
+					<form className="form-login">
+						<div className="form-input">
+							<input
+								className="email-input"
+								type="email"
+								value={email}
+								placeholder="usuario@email.com"
+								onChange={(e) => setEmail(e.target.value)}
+								required
+							></input>
+						</div>
+						<div className="form-input">
+							<input
+								className="password-input"
+								type={showPassword ? "text" : "password"}
+								value={password}
+								onChange={(e) => setPassword(e.target.value)}
+								placeholder="Contraseña"
+								id="passcode"
+								required
+							></input>
+							<span onClick={handleIcon}>
+								{showPassword ? (
+									<AiFillEyeInvisible className="eye-icon" />
+								) : (
+									<IoMdEye className="eye-icon" />
+								)}
+							</span>
+						</div>
+						<button className="login-button" onClick={handleLoginUser}>
+							Iniciar sesión
+						</button>
+						<div className="reset-sign">
+							<p className="reset">¡Olvidé mi contraseña!</p>
+							<p className="sign-in">
+								<Link to="/register">Registrarme</Link>
+							</p>
+						</div>
+					</form>
+				</div>
+			</div>
+			<Footer />
+		</>
+	);
+};
+
+export default Login;
---
+++ veterinary-clinic/src/pages/Register/Register.jsx
@@ -0,0 +1,277 @@
+import React, { useContext } from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import { IoMdArrowRoundBack } from "react-icons/io";
+import { MdOutlineError } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
+import "./Register.css";
+import { useState } from "react";
+import axios from "axios";
+import { UserContext } from "../../context/UserContext";
+import { RiDeleteBin6Line } from "react-icons/ri";
+
+const Register = () => {
+	const navigate = useNavigate();
+	const { setDataUser } = useContext(UserContext);
+
+	const [name, setName] = useState("");
+	const [lastname, setLastname] = useState("");
+	const [address, setAddress] = useState("");
+	const [phone, setPhone] = useState();
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState();
+
+	const [pet, setPet] = useState({
+		petName: "",
+		age: "",
+		gender: "",
+		petSpecies: "",
+	});
+
+	let [petsArray, setPetsArray] = useState([]);
+
+	const [error, setError] = useState(false);
+	const [errorRegisterMsj, setErrorRegisterMsj] = useState("");
+
+	const goBack = () => {
+		navigate(-1);
+	};
+
+	const inputPetsChange = (event) => {
+		const { name, value } = event.target;
+		setPet((prevPet) => ({
+			...prevPet,
+			[name]: value,
+		}));
+	};
+
+	const savingPets = (event) => {
+		event.preventDefault();
+		setPetsArray([...petsArray, pet]);
+		setPet({ petName: "", age: "", gender: "default", petSpecies: "default" });
+	};
+
+	const deletePet = (index) => {
+		let newPetsArray = petsArray.filter((_, i) => i !== index);
+		setPetsArray(newPetsArray);
+	};
+
+	const handleRegisterUser = (e) => {
+		e.preventDefault();
+		if (name && lastname && address && phone && email && password) {
+			setError(false);
+			axios
+				.post("http://localhost:8080/customer/addCustomer", {
+					name: name,
+					lastName: lastname,
+					address: address,
+					contactNumber: Number(phone),
+					email: email,
+					password: password,
+					pets: petsArray,
+				})
+				.then((response) => {
+					console.log("respuesta:", response);
+					if (response.status === 200 || response.status === 201) {
+						setDataUser(response.config.data);
+						setName("");
+						setLastname("");
+						setAddress("");
+						setPhone("");
+						setEmail("");
+						setPassword("");
+						goBack();
+					}
+				})
+				.catch((error) => console.log(error));
+		} else {
+			setError(true);
+			setErrorRegisterMsj("Debes completar todos los campos del formulario");
+		}
+	};
+	return (
+		<>
+			<Navbar />
+			<div className="icon-direction">
+				<IoMdArrowRoundBack className="arrow" onClick={goBack} />
+			</div>
+			<div className="ctn-register">
+				{error ? (
+					<div className="error-message-div">
+						<MdOutlineError className="error-icon" />
+						<p>{errorRegisterMsj}</p>
+					</div>
+				) : (
+					""
+				)}
+				<h1 className="register-title">Crea tu cuenta</h1>
+				<p className="register-description-title">
+					Completa un sencillo formulario, agrega a todas tus mascotas y
+					adéntrate en un mundo de amor en nuestra comunidad ♥
+				</p>
+				<form>
+					<div className="box-user-pets">
+						<div className="user">
+							<h2>Agrega tus datos</h2>
+							<div className="form-register">
+								<div className="section-form">
+									<div className="register-name">
+										<input
+											className="input-register-name"
+											type="text"
+											name="name"
+											value={name}
+											placeholder="Nombre"
+											onChange={(e) => setName(e.target.value)}
+											required
+										></input>
+									</div>
+									<div className="register-lastname">
+										<input
+											className="input-register-lastname"
+											type="text"
+											name="lastname"
+											value={lastname}
+											placeholder="Apellido"
+											onChange={(e) => setLastname(e.target.value)}
+											required
+										></input>
+									</div>
+								</div>
+								<div className="section-form">
+									<div className="register-address">
+										<input
+											className="input-register-address"
+											type="text"
+											name="address"
+											value={address}
+											placeholder="Direccion"
+											onChange={(e) => setAddress(e.target.value)}
+											required
+										></input>
+									</div>
+									<div className="register-phone">
+										<input
+											className="input-register-phone"
+											type="text"
+											name="phone"
+											value={phone}
+											placeholder="telefono"
+											onChange={(e) => setPhone(e.target.value)}
+											required
+										></input>
+									</div>
+								</div>
+								<div className="section-form">
+									<div className="register-email">
+										<input
+											type="email"
+											className="input-register-email"
+											name="email"
+											value={email}
+											placeholder="Correo electrónico"
+											onChange={(e) => setEmail(e.target.value)}
+											required
+										></input>
+									</div>
+									<div className="register-passcode">
+										<input
+											type="password"
+											className="input-register-passcode"
+											name="password"
+											value={password}
+											placeholder="Contraseña"
+											onChange={(e) => setPassword(e.target.value)}
+											required
+										></input>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="pets">
+							<h2>Agrega los datos de tu mascota</h2>
+							<div className="form-register">
+								<div className="petsArray">
+									{petsArray &&
+										petsArray.map((p, index) => {
+											return (
+												<div key={index} className="added-pet">
+													{p.petName}
+													<RiDeleteBin6Line
+														className="delete-pet"
+														onClick={() => deletePet(index)}
+													/>
+												</div>
+											);
+										})}
+								</div>
+
+								<article>
+									<div className="ctn-pet">
+										<div className="section-form-pets">
+											<div className="register-pet-section">
+												<input
+													className="input-pet"
+													type="text"
+													name="petName"
+													value={pet.petName}
+													placeholder="Nombre"
+													onChange={inputPetsChange}
+												></input>
+											</div>
+											<div className="register-pet-section">
+												<input
+													className="input-pet"
+													type="number"
+													name="age"
+													value={pet.age}
+													placeholder="Edad"
+													onChange={inputPetsChange}
+												></input>
+											</div>
+										</div>
+										<div className="section-form-pets">
+											<div className="register-pet-section">
+												<select
+													value={pet.gender}
+													name="gender"
+													onChange={inputPetsChange}
+												>
+													<option value="default">Género</option>
+													<option>Masculino</option>
+													<option>Femenino</option>
+												</select>
+											</div>
+											<div className="register-pet-section">
+												<select
+													value={pet.petSpecies}
+													name="petSpecies"
+													onChange={inputPetsChange}
+												>
+													<option value="default">Especie</option>
+													<option>Canino</option>
+													<option>Felino</option>
+												</select>
+											</div>
+										</div>
+									</div>
+									<button
+										type="button"
+										className="pets-button"
+										onClick={savingPets}
+									>
+										Agregar
+									</button>
+								</article>
+							</div>
+						</div>
+					</div>
+					<button className="register-button" onClick={handleRegisterUser}>
+						Crear cuenta
+					</button>
+				</form>
+			</div>
+		</>
+	);
+};
+
+export default Register;
