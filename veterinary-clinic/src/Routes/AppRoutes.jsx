3121c631ed655f393bcca022f70aa28b78fe4f1a
ArchSofia <sofiadiazvaldez@gmail.com>
2023-09-27 11:38:15 -0300

    Agrega las rutas y paginas login y registro

--- veterinary-clinic/src/Routes/AppRoutes.jsx
+++ veterinary-clinic/src/Routes/AppRoutes.jsx
@@ -1,17 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from '../pages/Home/Home';
-import { UserProvider } from '../context/UserContext';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home/Home";
+import Login from "../pages/Login/login";
+import Register from "../pages/Register/Register";
+import { UserProvider } from "../context/UserContext";
 
 const AppRoutes = () => {
-  return (
-    <UserProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-    </UserProvider>
-  )
-}
+	return (
+		<UserProvider>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/register" element={<Register />} />
+				</Routes>
+			</BrowserRouter>
+		</UserProvider>
+	);
+};
 
 export default AppRoutes;
