7b9dd63ef86101c84eab4f878f71f87dc4b7ef40
vevis <vevisvs@gmail.com>
2023-11-02 01:00:00 -0300

    hide items list at certain location

--- veterinary-clinic/src/components/Navbar/Navbar.jsx
+++ veterinary-clinic/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,8 @@
-import React, {  useContext, useState } from "react";
+import React, {  useContext, useState, useEffect } from "react";
 import "../Navbar/Navbar.css";
 import { FaBars } from "react-icons/fa";
 import { IoMdClose } from "react-icons/io";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { UserContext } from "../../context/UserContext";
 import { Link } from "react-router-dom";
 
@@ -10,10 +10,22 @@ const Navbar = () => {
 	const navigate = useNavigate();
 	const [sidebarOpen, setSidebarOpen] = useState(false);
 	const {isUserLogged} = useContext(UserContext)
+	const [hideMenu, setHideMenu] = useState(false);
+
+	const location = useLocation();
+	const isReservationRoute = location.pathname === "/reservation";
 
 	const goHome = () => {
 		navigate("/");
 	};
+
+	useEffect(() => {
+		if (isReservationRoute) {
+		  setHideMenu(true);
+		} else {
+		  setHideMenu(false);
+		}
+	  }, [isReservationRoute]);
 
 	return (
 		<div className="navbar" id="home">
@@ -37,27 +49,32 @@ const Navbar = () => {
 				<div className="icon-close" onClick={() => setSidebarOpen(false)}>
 					<IoMdClose />
 				</div>
-				<ul className={sidebarOpen ? "sidebar-list" : "menu-list"}>
-						{
-							isUserLogged &&
-							<button className='info-button'><Link to="/reservation">Reservar una cita</Link></button>
-						}
-					<li>
-						<a href="#">Home</a>
-					</li>
-					<li>
-						<a href="#services">Servicios</a>
-					</li>
-					<li>
-						<a href="#vets">Veterinarios</a>
-					</li>
-					<li>
-						<a href="#about">About</a>
-					</li>
-					<li>
-						<a href="#contact">Contacto</a>
-					</li>
-				</ul>
+				{hideMenu ? (
+					""
+					) : (
+					<ul className={sidebarOpen ? "sidebar-list" : "menu-list"}>
+						{isUserLogged && (
+						<button className="info-button">
+							<Link to="/reservation">Reservar una cita</Link>
+						</button>
+						)}
+						<li>
+							<a href="#">Home</a>
+						</li>
+						<li>
+							<a href="#services">Servicios</a>
+						</li>
+						<li>
+							<a href="#vets">Veterinarios</a>
+						</li>
+						<li>
+							<a href="#about">About</a>
+						</li>
+						<li>
+							<a href="#contact">Contacto</a>
+						</li>
+					</ul>
+					)}
 			</div>
 		</div>
 	);
