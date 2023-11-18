75d184892673efdca6aff81e31510f86f1a7e1d2
vevis <vevisvs@gmail.com>
2023-11-17 21:05:44 -0300

    changes client fixed

--- veterinary-clinic/src/components/Information/Information.jsx
+++ veterinary-clinic/src/components/Information/Information.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../Information/Information.css";
-import veterinarian from "../../assets/veterinarian-photo.png";
+import veterinarian from "../../assets/guada.jpg";
 
 const Information = () => {
 	return (
@@ -9,15 +9,12 @@ const Information = () => {
 			<div className="info-description">
 				<h1 className="text-parrafo">
 					<div>
-						Los mejores médicos veterinarios de todas las especialidades
+						Nuestra misión
 					</div>
 				</h1>
 				<p className="text-parrafo-p">
 					{" "}
-					Todos los veterinarios son expertos en sus especialidades y graduados
-					de las mejores universidades de veterinaria.<br></br> Nuestro staff
-					experimentado trabaja en equipo para brindarle a su mascota la mejor
-					atención posible.
+					Somos una clínica veterinaria que brinda un servicio veterinario integral, como así también productos para mascotas en la localidad de Pilar, integrada por un equipo de profesionales en constante formación, con el objetivo de brindar un servicio de excelencia y tecnología de diagnóstico aplicada. <br/>Nuestra norte es trabajar bajo una premisa: el trato humanitario hacia nuestros pacientes, generando y transfiriendo valor hacia los tutores de mascotas.
 				</p>
 			</div>
 		</div>
--- veterinary-clinic/src/components/Services/Services.jsx
+++ veterinary-clinic/src/components/Services/Services.jsx
@@ -7,15 +7,14 @@ const Services = () => {
   return (
     <div className='ctn-services' id='services'>
         <div className='services-title'>
-            <h1>¿Qué servicios ofrecemos?</h1>
-            <span className='offer-text'>Estamos a tu servicio con todo el equipo médico y personal que necesites</span>
+            <h1>Brindamos un servicio veterinario integral</h1>
         </div>
         <div className='ctn-services-info'>
           <ServiceCard />
           <div className="card-pet-shop">
             <img src="/src/assets/petshop.png" alt="petshop" />
             <h2 className='card-title'>Pet Shop</h2>
-            <p className='card-description'>Alimentos, accesorios y medicación para tus mascotass</p>
+            <p className='card-description'>Alimentos súper Premium, Farmacia veterinaria y todo lo que necesita tu mascota</p>
 			    </div>
         </div>
       
